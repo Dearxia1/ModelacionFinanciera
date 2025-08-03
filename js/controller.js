cccf3c7290e3bedb4087f07bebe83c074366f104
dano <danielmax547@gmail.com>
2025-08-03 17:24:02 -0500

    Refactor: Code practices renew & adding TPAA|TPNA

--- js/controller.js
+++ js/controller.js
@@ -1,46 +1,50 @@
-// controlador.js
-import { calcularTasaEfectiva,calculartasaPeriodica, calcularTasaPeriodicaAlNegocio,calcularTasaPeriodicaNominal} from './model.js';
+// controller.js
+import { calcTE, calcTP, calcTPA, calcTPN, calcTPAA, calcTPNA } from './model.js';
 
-function convertirTasa() {
-  const tasa = parseFloat(document.getElementById('tasa').value);
-  const n = parseInt(document.getElementById('periodos').value);
+function convertirTN() {
+  const tn = parseFloat(document.getElementById('inpTN').value);
+  const n = parseInt(document.getElementById('selN1').value);
 
-  const resultadoElemento = document.getElementById('resultado');
-  const resultadoElemento2 = document.getElementById('resultadoTasaPeriodica');
+  const resTE = document.getElementById('resTE');
+  const resTP = document.getElementById('resTP');
 
-  if (isNaN(tasa) || isNaN(n) || n <= 0) {
-    resultadoElemento.textContent = 'Ingrese valores válidos.';
+  if (isNaN(tn) || isNaN(n) || n <= 0) {
+    resTE.textContent = 'Ingrese valores válidos.';
     return;
   }
 
-  const tasaPeriodica = calculartasaPeriodica(tasa,n);
-  const tasaEfectiva = calcularTasaEfectiva(tasa, n);
-  resultadoElemento.textContent = `Tasa efectiva: ${tasaEfectiva.toFixed(2)}%`;
-  resultadoElemento2.textContent = `Tasa periodica ${tasaPeriodica.toFixed(2)}%`;
+  const tp = calcTP(tn, n);
+  const te = calcTE(tn, n);
+
+  resTE.textContent = `Tasa efectiva: ${te.toFixed(2)}%`;
+  resTP.textContent = `Tasa periódica: ${tp.toFixed(2)}%`;
 }
 
-document.getElementById("convertirBtn1").addEventListener("click", convertirTasa);
+document.getElementById("btnConvertirTN").addEventListener("click", convertirTN);
 
+function convertirTE() {
+  const te = parseFloat(document.getElementById('inpTE').value);
+  const n = parseFloat(document.getElementById('selN2').value);
 
-function convertirTasa2(){
+  const resTPA = document.getElementById('resTPA');
+  const resTPNV = document.getElementById('resTPNV');
+  const resTPAA = document.getElementById('resTPAA');
+  const resTPNA = document.getElementById('resTPNA');
 
-let tasa = parseFloat(document.getElementById(`tasaEfectiva`).value);
-let n = parseFloat(document.getElementById(`periodos2`).value);
-
-
-  if (isNaN(tasa) || isNaN(n) || n <= 0) {
-    resultadoElemento.textContent = 'Ingrese valores válidos.';
+  if (isNaN(te) || isNaN(n) || n <= 0) {
+    resTPA.textContent = 'Ingrese valores válidos.';
     return;
   }
 
-  let TasaAplicada = calcularTasaPeriodicaAlNegocio(tasa,n)
-  let TasaPeriodicaNominal = calcularTasaPeriodicaNominal(TasaAplicada,n)
-  resultadoTasaPeriodicaAplicadaAlNegocio.textContent = TasaAplicada.toFixed(2);
-  resultadoTasaPeriodicaNominal.textContent = TasaPeriodicaNominal.toFixed(2);
-  console.log(resultadoTasaPeriodicaNominal)
+  const tpa = calcTPA(te, n);
+  const tpn = calcTPN(tpa, n);
+  const tpaa = calcTPAA(tpa);
+  const tpna = calcTPNA(tpaa,n)
 
+  resTPA.textContent = `TP aplicada vencida: ${tpa.toFixed(2)}%`;
+  resTPNV.textContent = `TP nominal vencida: ${tpn.toFixed(2)}%`;
+  resTPAA.textContent = `TP aplicada anticipada: ${tpaa.toFixed(2)}%`;
+  resTPNA.textContent = `TP nominal anticipada: ${tpna.toFixed(2)}%`;
 }
 
-
-document.getElementById("convertirBtn2").addEventListener("click", convertirTasa2);
-
+document.getElementById("btnConvertirTE").addEventListener("click", convertirTE);
--- js/model.js
+++ js/model.js
@@ -1,18 +1,25 @@
-// modelo.js
-export function calcularTasaEfectiva(tasaNominal, periodos) {
-  return (Math.pow(1 + (tasaNominal / 100) / periodos, periodos) - 1) * 100;
+// model.js
+
+export function calcTE(tn, n) {
+  return (Math.pow(1 + (tn / 100) / n, n) - 1) * 100;
 }
 
-
-export function calculartasaPeriodica(tasaNominal,periodos){
-return((tasaNominal/100) / periodos) * 100
+export function calcTP(tn, n) {
+  return ((tn / 100) / n) * 100;
 }
 
-
-export function calcularTasaPeriodicaAlNegocio(tasaEfectiva,periodos){
-  return ((Math.pow(1 + (tasaEfectiva / 100), 1 / periodos )) - 1) * 100
+export function calcTPA(te, n) {
+  return (Math.pow(1 + (te / 100), 1 / n) - 1) * 100;
 }
 
-export function calcularTasaPeriodicaNominal(tasaPeriodicaAlNegocio,periodos){
-return ((tasaPeriodicaAlNegocio / 100 ) * periodos) * 100;
+export function calcTPN(tpa, n) {
+  return ((tpa / 100) * n) * 100;
+}
+
+export function calcTPAA(te){
+  return ((te / 100)/(1 + (te / 100))) * 100;
+}
+
+export function calcTPNA(tpa,n){
+  return (((tpa) * (n)));
 }
